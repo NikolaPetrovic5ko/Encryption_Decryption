b44db15509a463fbaa0caf28aace8ffa2c842ed1
NikolaPetrovic5ko <npetrovic155@gmail.com>
2018-06-04 11:24:50 +0200

    Created main file

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char message[100];
+    
+          printf("====Enter message for encryption====\n");
+          printf("NOTE: First letter must be 'a', than comes white space\nand than you enter your message\nMESSAGE:");
+          gets(message);
+     
+          return 0;
+}
