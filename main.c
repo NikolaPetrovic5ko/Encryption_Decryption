d32b6e53693218829537cb5a205f02f1a602f219
NikolaPetrovic5ko <npetrovic155@gmail.com>
2018-06-04 16:29:40 +0200

    Added function for decryption

--- main.c
+++ main.c
@@ -3,14 +3,20 @@
 #include <string.h>
 
 void encryption(char *message);
+void decryption(char *message);
 
 int main()
 {
     char message[100];
+
       printf("====Enter message for encryption====\n");
-      printf("NOTE: First letter must be 'a', than comes white space\nand than you enter your message\nMESSAGE:");
+      printf("NOTE: First letter must be 'a', than comes white space\nand than you enter your message\nMESSAGE: ");
       gets(message);
       encryption(&message[0]);
+      getchar();
+      printf("====Enter message for decryption====\nMESSAGE: ");
+      gets(message);
+      decryption(&message[0]);
 
     return 0;
 }
@@ -97,4 +103,88 @@ void encryption(char *message){
         }
     }
     printf("=====ENCRYPTED MESSAGE=====\n%s\n", encrypted_message);
+}
+
+void decryption(char *message){
+
+    char decrypted_message[100] = "";
+
+    int n1 = 10;
+    int n2 = 9;
+    int n3 = 7;
+
+    char row1_of_letters[10] = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'};
+    char row2_of_letters[9] = {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'};
+    char row3_of_letters[7] = {'z', 'x', 'c', 'v', 'b', 'n', 'm'};
+
+    int iterator1 = 0;
+    int iterator2 = 0;
+    int char_position;
+    int is_char_found;
+    int distance;
+    int length_of_message = strlen(message);
+
+    char letter_for_decryption;
+
+    char *row1 = row1_of_letters;
+    char *row2 = row2_of_letters;
+    char *row3 = row3_of_letters;
+    char *ptrMessage = message;
+    char *decrypted_msg = decrypted_message;
+
+    letter_for_decryption = *ptrMessage;
+    while( row2_of_letters[distance] != letter_for_decryption ){
+        distance = distance + 1;
+    }
+
+    for(iterator1 = 0; iterator1 < length_of_message; iterator1++){
+    ///Decryption with first row
+        is_char_found = 0;
+        for(iterator2 = 0; iterator2 < n1; iterator2++){
+            if(*(ptrMessage + iterator1) == *(row1 + iterator2)){
+                if( (iterator2 - distance) < 0 ) {
+                    char_position = n1 - (distance - iterator2);
+                    *(decrypted_msg + iterator1) = *(row1 + char_position);
+                    is_char_found = 1;
+                } else {
+                    *(decrypted_msg + iterator1) = *(row1 + iterator2 - distance);
+                    is_char_found = 1;
+                }
+            }
+        }
+    ///Decryption with second row
+        if(is_char_found == 0) {
+            for(iterator2 = 0; iterator2 < n2; iterator2++){
+            if(*(ptrMessage + iterator1) == *(row2 + iterator2)){
+                if( (iterator2 - distance) < 0 ) {
+                    char_position = n2 - (distance - iterator2);
+                    *(decrypted_msg + iterator1) = *(row2 + char_position);
+                    is_char_found = 1;
+                } else {
+                    *(decrypted_msg + iterator1) = *(row2 + iterator2 - distance);
+                    is_char_found = 1;
+                }
+            }
+        }
+    ///Decryption with third row
+        if(is_char_found == 0) {
+                for(iterator2 = 0; iterator2 < n3; iterator2++){
+            if(*(ptrMessage + iterator1) == *(row3 + iterator2)){
+                if( (iterator2 - distance) < 0 ) {
+                    char_position = n3 - (distance - iterator2);
+                    *(decrypted_msg + iterator1) = *(row3 + char_position);
+                    is_char_found = 1;
+                } else {
+                    *(decrypted_msg + iterator1) = *(row3 + iterator2 - distance);
+                    is_char_found = 1;
+                }
+            }
+        }
+        if(is_char_found == 0) {
+            *(decrypted_msg + iterator1) = *(ptrMessage + iterator1);
+                }
+            }
+        }
+    }
+    printf("=====DECRYPTED MESSAGE=====\n%s\n", decrypted_message);
 }
